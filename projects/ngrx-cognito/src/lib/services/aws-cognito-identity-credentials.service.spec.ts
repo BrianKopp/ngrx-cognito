894e41d976515faefde4fe9b9de807840516c971
Brian Kopp <briankopp.usa@gmail.com>
2018-12-16 22:41:18 -0700

    pass tests on aws-cognito-identity-credentials.service

--- projects/ngrx-cognito/src/lib/services/aws-cognito-identity-credentials.service.spec.ts
+++ projects/ngrx-cognito/src/lib/services/aws-cognito-identity-credentials.service.spec.ts
@@ -1,9 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AwsCognitoIdentityCredentialsService } from './aws-cognito-identity-credentials.service';
+import { CognitoConfigService } from './cognito-config.service';
+import { CognitoFacade } from '../state/cognito.facade';
+import { StoreModule } from '@ngrx/store';
 
 describe('AwsCognitoIdentityCredentialsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [StoreModule.forRoot({})],
+      providers: [{ provide: CognitoConfigService, useValue: {} }, CognitoFacade]
+    })
+  );
 
   it('should be created', () => {
     const service: AwsCognitoIdentityCredentialsService = TestBed.get(AwsCognitoIdentityCredentialsService);
